2698bcec65f1ce8b7d5dd6f84486089911109967
annakulova <aakulovaa03@gmail.com>
2024-03-18 21:59:15 +0300

    Считывание из файла
    Добавление возможности считывать из файла строку

--- src/main/java/org/strategy/App.java
+++ src/main/java/org/strategy/App.java
@@ -1,9 +1,14 @@
 package org.strategy;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws FileNotFoundException {
+        File file = new File("src/main/textFile.txt");
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        System.out.println(line);
         Parser parser = new Parser(new ParseOnlyLetters());
         parser.parsing();
     }
