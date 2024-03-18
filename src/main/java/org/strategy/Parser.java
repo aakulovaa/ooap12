d4cec6a6ec74ae7bc171800413552b439268f449
annakulova <aakulovaa03@gmail.com>
2024-03-18 21:27:48 +0300

    Метод стратегия
    определение представления того, как это должно быть

--- src/main/java/org/strategy/Parser.java
+++ src/main/java/org/strategy/Parser.java
@@ -1,12 +1,23 @@
 package org.strategy;
 
+/**
+ * Класс Парсер для синтаксического анализа данных из файла
+ * со свойством <b>parse</b>
+ */
 public class Parser {
     private Parse parse;
 
+    /**
+     * Конструктор для установки значения parse
+     * @param parse - элемент одного из классов, реализующих интерфейс
+     */
     public Parser(Parse parse) {
         this.parse = parse;
     }
 
+    /**
+     * Метод считывания из файла по определенному условию
+     */
     public void parsing(){
         parse.parse();
     }
